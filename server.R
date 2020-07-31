45b144c96407f5bbc705aa2e4eda9cc97f3b959c
Jonathan Sidi <yonis@metrumrg.com>
2020-07-31 17:15:28 -0400

    update github

--- server.R
+++ server.R
@@ -1,32 +1,11 @@
 shinyServer(function(input, output, session) {
-    
-  # shiny::observeEvent(input$run,{
-  #   
-  #   new_est <- tidynm::read_extensions(file.path(input$path,input$run),
-  #                           exts = 'ext',
-  #                           est_ind = identity)
-  #   if(nrow(new_est)>0){
-  #   
-  #     new_est %>%
-  #       dplyr::pull(estimation_index)%>%
-  #       as.numeric()
-  #     
-  #     shiny::updateSelectInput(session = session,
-  #                              inputId = 'est',
-  #                              choices = new_est,
-  #                              selected = new_est[1])
-  #       
-  #   }
-  #   
-  #   
-  # })
-  
+
   shiny::observeEvent(c(input$run),{
     output$plot <- shiny::renderPlot({
       ext_status_plot(data()(),stat = input$panel)
     })
   })
-    
+
     data <- shiny::eventReactive(input$run,{
       shiny::reactivePoll(
         1000, session,
@@ -42,14 +21,14 @@ shinyServer(function(input, output, session) {
         valueFunc = function() {
           ext_status_read(file.path(input$path,input$run),est_ind = input$est)
         }
-      )  
+      )
     })
-    
-    
+
+
     shiny::observeEvent(input$path,{
-      
+
       runs <- basename(list.dirs(input$path,recursive = FALSE))
-      
+
       output$run <- renderUI({
         shiny::selectInput(
           inputId = 'run',
@@ -58,12 +37,11 @@ shinyServer(function(input, output, session) {
           selected = tail(runs,1)
         )
       })
-      
+
     })
-    
+
     shiny::observeEvent(input$qt, {
       shiny::stopApp()
     })
-    
+
 })
-  
